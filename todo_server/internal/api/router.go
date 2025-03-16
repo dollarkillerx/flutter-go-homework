9c98304a3c8dfb553081f0eed4188ec2e0c27d29
wang-ye-v0 <wang.ye@systemi.co.jp>
2025-03-16 17:52:38 +0900

    init project

--- todo_server/internal/api/router.go
+++ todo_server/internal/api/router.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	"todo_server/internal/middleware"
 )
 
 // Router sets up the API routes for the server.
@@ -23,15 +22,15 @@ func (a *Server) Router() {
 		v1.POST("/public/register", NewUserController(a.storage).register) // 用户注册
 		v1.POST("/public/login", NewUserController(a.storage).login)       // 用户登录
 
-		private := v1.Group("/private", middleware.Auth(a.conf.JWTSecretKey))
-		{
-			private.GET("/tasks", a.getTasks)   // 获取任务列表
-			private.POST("/task", a.createTask) // 创建任务
-			private.PUT("/task", a.updateTask)  // 更新任务
-
-			// 当app前台活跃的时候使用拉模式
-			private.GET("/notice", a.notice) // 获取通知
-		}
+		//private := v1.Group("/private", middleware.Auth(a.conf.JWTSecretKey))
+		//{
+		//	private.GET("/tasks", a.getTasks)   // 获取任务列表
+		//	private.POST("/task", a.createTask) // 创建任务
+		//	private.PUT("/task", a.updateTask)  // 更新任务
+		//
+		//	// 当app前台活跃的时候使用拉模式
+		//	private.GET("/notice", a.notice) // 获取通知
+		//}
 	}
 }
 
